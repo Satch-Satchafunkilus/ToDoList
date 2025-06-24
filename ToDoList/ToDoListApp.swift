743b4ebf49d2d4701b34078b1fe574531e7518ef
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-24 09:42:42 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Tushar Munge on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
